37bac7c8b1d5c1f6c11872e11a12661aa98dbca2
Sang Nguyen <nnsang24@gmail.com>
2021-12-04 17:14:43 +0700

    make slidebar smooth

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,10 +1,21 @@
-const SideBar = () => {
+import { isMobile } from 'mobile-device-detect';
+
+const toggleSidebar = () => {
 	const sidebarBtn = document.querySelector('.sidebar-arrow');
 	const sidebar = document.querySelector('.sidebar');
+	const sidebarArrow = <HTMLElement>document.querySelector('.sidebar-arrow');
 
 	sidebarBtn?.addEventListener('click', () => {
 		sidebar?.classList.toggle('is-collapse');
-	})
+	});
+
+	if (isMobile) {
+		sidebarArrow.style.display = "none";
+	}
+};
+
+const SideBar = () => {
+	toggleSidebar();
 };
 
 export { SideBar };
